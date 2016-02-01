e247457c6a971c64bf6c980e7b6ceab9c10c35dd
chausen <clayhausen@gmail.com>
2016-02-01 18:03:27 -0500

    Added Entity to its own directory and further developed skeleton

---
+++ src/models/Entity/Entity.java
@@ -0,0 +1,63 @@
+package models.Entity;
+
+
+public class Entity {
+    /* ATTRIBUTES */
+    private String name;
+    private Occupation occupation;
+    private Stats stats;
+    private Inventory inventory;
+    private Equipment equipment;
+    
+    /* METHODS */
+
+    /* move(:Direction)
+    ** Description pending...
+    */
+    void move(Direction direction) {}
+    /* modifyStats(:StatModifier)
+    ** Description pending...
+    */
+    void modifyStats(StatModifier statMod) {}
+    /* takeDamage(:int):int
+    ** Parameters
+    ** in: damage taken
+    ** out: Entity's remaining life after taking damage (currentLife)
+    */
+    int takeDamage(int amount) {}
+    /* healDamage(:int): int
+    ** Parameters
+    ** in: damage healed
+    ** out: Entity's remaining life after healing damage (currentLife)
+    */
+    int healDamage(int amount) {}
+    /* loseLife: int
+    ** Parameters
+    ** out: number of lives Entity has remaining after losing one (livesLeft)
+    */
+    int loseLife() {}
+    /* gainXp(:int)
+    ** Parameters
+    ** in: the amount of XP gained
+    */
+    void gainXp(int amount) {}
+    /* equip(:Item): boolean
+    ** Parameters
+    ** in: the Item to equip
+    ** out: a boolean representing whether or not the equip action was successful
+    */
+    boolean equip(Item item) {}
+    /* unequip(:Item): boolean
+    ** Parameters
+    ** in: the Item to unequip
+    ** out: a boolean representing whether or not the unequip action was successful
+    */
+    boolean unequip(Item item) {}
+    /* dropItem(:Item): boolean 
+    ** Parameters
+    ** in: the Item to drop
+    ** out: a boolean representing whether or not the drop item action was successful
+    */
+    boolean dropItem(Item item) {}        
+    
+}
