ed9d1c865bd41c912e954c7144698ce336b62ca7
Matthew D Diaz <matthewdiaz10@ufl.edu>
2016-02-03 19:26:30 -0500

    Modify MainMenu to have create Options instead of Selections

--- src/models/menus/MainMenu.java
+++ src/models/menus/MainMenu.java
@@ -2,13 +2,12 @@ package models.menus;
 
 import java.util.ArrayList;
 public class MainMenu extends Menus{
-  private Selection currentlySelected;
 
   /*
    * Default constructor
    */
   public MainMenu(){
-    super(new Selection[]{Selection.NEWGAME, Selection.LOADGAME, Selection.EXIT},
-          new ArrayList<Listener>());
+    super(new Option[]{new NewGameOption(), new LoadGameOption(),
+          new ExitOption()}, new ArrayList<Listener>());
   }
 }
