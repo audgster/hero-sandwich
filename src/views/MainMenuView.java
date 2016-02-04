b4aeb989fec17daa5156c550ff395208857db533
Brandon <bduong30@ufl.edu>
2016-02-03 23:29:25 -0500

    gameWindow does not implement Listener nor update the views anymore

--- src/views/MainMenuView.java
+++ src/views/MainMenuView.java
@@ -4,32 +4,38 @@ import javax.swing.*;
 import java.awt.*;
 import models.menus.*;
 
-
 public class MainMenuView extends View{
-    private Image sandwich;
-    private JLabel gameTitle, newGame, loadGame, exitGame, imagePanel;
-    private JPanel bottomPanel;
+    private JLabel gameTitle, newGame, loadGame, exitGame, imageLabel;
+    private JPanel textPanel, centerPanel;
     private MainMenu menuModel;
 
     public MainMenuView(Menus model){
         menuModel = (MainMenu) model;
         menuModel.addListener(this);
         setLayout(new BorderLayout());
-        bottomPanel = new JPanel();
-            bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.PAGE_AXIS));
-        imagePanel = new JLabel(new ImageIcon("../images/big-sandwich.gif"));
+        centerPanel = new JPanel();
+        textPanel = new JPanel();
+            textPanel.setLayout(new BoxLayout(textPanel, BoxLayout.PAGE_AXIS));
+        initializeText();
+        render();
+    }
+
+    public void initializeText(){
+        imageLabel = new JLabel(new ImageIcon("../images/big-sandwich.gif"));
         gameTitle = new JLabel("Hero Sandwich");
+                gameTitle.setHorizontalAlignment(SwingConstants.CENTER);
         newGame = new JLabel("New Game");
         loadGame = new JLabel("Load Game");
         exitGame = new JLabel("Exit Game");
-        render();
     }
 
     public void render(){
         setBackground(Color.WHITE);
-            bottomPanel.setBackground(Color.WHITE);
+        textPanel.setBackground(Color.WHITE);
+        centerPanel.setBackground(Color.WHITE);
 
         newGame.setBackground(Color.GRAY);
+                newGame.setOpaque(true);
         loadGame.setBackground(Color.GRAY);
         exitGame.setBackground(Color.GRAY);
 
@@ -45,13 +51,13 @@ public class MainMenuView extends View{
 
         add(gameTitle, BorderLayout.PAGE_START);
         gameTitle.setPreferredSize(new Dimension(1300, 100));
-        add(imagePanel, BorderLayout.CENTER);
-            bottomPanel.add(newGame);
-            bottomPanel.add(loadGame);
-            bottomPanel.add(exitGame);
-        add(bottomPanel, BorderLayout.CENTER_LEFT);
+            textPanel.add(newGame);
+            textPanel.add(loadGame);
+            textPanel.add(exitGame);
+        add(textPanel, BorderLayout.LINE_START);
+        add(imageLabel, BorderLayout.LINE_END);
+        add(centerPanel, BorderLayout.CENTER);
 
-        newGame.setOpaque(true);
         setVisible(true);
     }
 
