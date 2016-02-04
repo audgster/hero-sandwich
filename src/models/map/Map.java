6e2e21e0e60153540c8de85d8505152c03d034f5
Clay T. Hausen <chausen@stormx.cise.ufl.edu>
2016-02-04 13:53:48 -0500

    Added basic functionality for Map & Position...didn't have time to test, will later on my own machine...

--- src/models/map/Map.java
+++ src/models/map/Map.java
@@ -1,5 +1,17 @@
 package models.map;
 
-public class Map {
+import util.Position;
+import java.util.List;
+import java.util.ArrayList;
 
+public class Map {
+  private ArrayList< ArrayList<Tile> > grid;
+  /* Constructor: Create an NxN grid */
+  Map(int N) {
+    grid = new ArrayList( new ArrayList(N) );
+  }
+
+  public Tile getTileAt(Position p) {
+    return grid.get( p.getX().getY() );
+  }
 }
--- src/util/Position.java
+++ src/util/Position.java
@@ -2,6 +2,27 @@ package util;
 
 public class Position
 {
+  private int x;
+  private int y;
+
+  Position(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  @Override
+  public String toString() {
+    String str = "Position [" + x + "," + y + "]";
+    return str;
+  }
     // Audrey: need this for movement
     // Given a position and a direction, returns a new position
     public Position incrementPosition(Direction directionMoved) {return new Position();}
