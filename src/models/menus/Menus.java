ce05a01cefd88a1a8ae55162543a37947eb1be4e
Matthew D Diaz <matthewdiaz10@ufl.edu>
2016-02-03 19:17:55 -0500

    Change Selections to Options for Menu.java

--- src/models/menus/Menus.java
+++ src/models/menus/Menus.java
@@ -2,12 +2,12 @@ package models.menus;
 import java.util.ArrayList;
 
 public abstract class Menus{
-  private Selection currentlySelected;
-  private Selection[] listOfOptions;
+  private Option currentlySelected;
+  private Option[] listOfOptions;
   private ArrayList<Listener> listenerList;
   private int listPosition;
 
-  protected Menus(Selection[] listOfOptions, ArrayList<Listener> listenerList){
+  protected Menus(Option[] listOfOptions, ArrayList<Listener> listenerList){
     this.listOfOptions = listOfOptions;
     this.listenerList = listenerList;
     listPosition = 0;
@@ -53,11 +53,11 @@ public abstract class Menus{
   }
 
   //get methods
-  public Selection[] getListOfOptions(){
+  public Option[] getListOfOptions(){
     return listOfOptions;
   }
 
-  public Selection getCurrentlySelected(){
+  public Option getCurrentlySelected(){
     return currentlySelected;
   }
 }
