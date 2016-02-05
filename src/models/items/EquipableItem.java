5914a400045d24915bad38d196233387536aaa08
Brandon <bduong30@ufl.edu>
2016-02-05 17:29:39 -0500

    Equipable items have occupation and stat restrictions

--- src/models/items/EquipableItem.java
+++ src/models/items/EquipableItem.java
@@ -1,11 +1,28 @@
 package models.items;
 
-import models.entities.Entity;
+import models.entities.*;
 
 public class EquipableItem extends TakeableItem
 {
+	StatModifiers statRestrictions = new StatModifiers();
+	String occupRestriction = "";
     @Override
     public boolean executeInteraction(Entity entity) {
         return false;
+    }
+    public StatModifiers setStatRestrictions(StatModifiers s){
+    	statRestrictions = s;
+    }
+
+    public String setOccupationRestriction(String s){
+    	occupRestriction = s;
+    }
+
+    public StatModifiers getStatRestrictions(){
+    	return statRestrictions;
+    }
+
+    public String getOccupationRestriction(){
+    	return occupRestriction;
     }
 }
