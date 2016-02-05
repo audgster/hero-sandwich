61b96cc916a1325aef4e91e55e08539b2a39af8f
mburgunder <mitchellburgunder@yahoo.com>
2016-02-05 16:20:12 -0500

    can get list of items and AOEs from a tile

--- src/models/map/Tile.java
+++ src/models/map/Tile.java
@@ -1,23 +1,40 @@
 package models.map;
 
 import util.TerrainGroup;
+import models.map.areaofeffect.*;
+import models.items.*;
+import java.util.*;
 
 public class Tile
 {
     private TerrainGroup terrainType;
+	private ArrayList<Item> itemList;
+	private ArrayList<AreaOfEffect> aoeList;
 
     public Tile()
     {
 	    this.terrainType = TerrainGroup.GROUND;
+		this.itemList = new ArrayList<Item>();
+		this.aoeList = new ArrayList<AreaOfEffect>();
     }
 
     public Tile(TerrainGroup terrainType)
     {
         this.terrainType = terrainType;
+		this.itemList = new ArrayList<Item>();
+		this.aoeList = new ArrayList<AreaOfEffect>();
     }
 
     public String getTerrainType()
     {
         return this.terrainType.toString().toLowerCase();
     }
+	
+	public ArrayList<Item> getListOfItems(){
+		return itemList;
+	}
+	
+	public ArrayList<AreaOfEffect> getListOfAOEs(){
+		return aoeList;
+	}
 }
