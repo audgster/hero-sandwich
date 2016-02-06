f34caa28cf96966286f1e0bbe488e40fb54813bd
audgster <ahs3028@gmail.com>
2016-02-06 17:48:01 -0500

    wired up tile interactor

--- src/models/map/interactions/TileInteractionDirector.java
+++ src/models/map/interactions/TileInteractionDirector.java
@@ -1,10 +1,14 @@
 package models.map.interactions;
 
 import models.entities.Entity;
+import models.items.Item;
+import models.map.areaofeffect.AreaOfEffect;
 import models.map.interactions.interfaces.IInteractionHandler;
 import models.map.interactions.interfaces.ITileInteractionDirector;
 import models.map.Tile;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class TileInteractionDirector implements ITileInteractionDirector
@@ -12,8 +16,30 @@ public class TileInteractionDirector implements ITileInteractionDirector
     @Override
     public boolean executeInteraction(Tile tile, Entity entity)
     {
-        return false;
+        Collection<IInteractionHandler> handlers = new ArrayList<>();
+
+        for (AreaOfEffect aoe : tile.getAllAoE())
+        {
+            handlers.add(aoe);
+        }
+
+        for (Item item : tile.getAllItems())
+        {
+            handlers.add(item);
+        }
+
+        boolean success = true;
+
+        for (IInteractionHandler handler : handlers)
+        {
+            success = success && executeHandler(handler, entity);
+        }
+
+        return success;
     }
 
-    private boolean executeHandler(IInteractionHandler handler, Entity entity) { return false; }
+    private boolean executeHandler(IInteractionHandler handler, Entity entity)
+    {
+        return handler.executeInteraction(entity);
+    }
 }
