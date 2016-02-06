eaff00a3bb630d19cd1569e4dd93cd5a4cd31027
Brandon <bduong30@ufl.edu>
2016-02-06 17:36:11 -0500

    added occupation pics to custom view

--- src/views/CustomizeView.java
+++ src/views/CustomizeView.java
@@ -34,10 +34,22 @@ public class CustomizeView extends View{
 	protected void render(){
 		removeAll();
 
+			BufferedImage smasherImage = null, sneakImage = null, summonerImage = null;
+			try{
+				smasherImage = ImageIO.read(new File("../images/smasher.gif"));
+				sneakImage = ImageIO.read(new File("../images/smasher.gif"));
+				summonerImage = ImageIO.read(new File("../images/smasher.gif"));
+			}
+			catch(IOException e){
+				System.out.println("could not find any files!!!!!!!!!!!!!!!!!!!");
+			}
+			ImageIcon icon = new ImageIcon(smasherImage);
+
 		for(int i = 0; i < options.length; i++){
 			JLabel label = new JLabel(options[i]);
+			label.setIcon(icon);
 			label.setAlignmentX(Component.CENTER_ALIGNMENT);
-			label.setBackground(Color.BLACK);
+			label.setBackground(Color.GREEN);
 			label.setFont(menuFont);
 			if(currentIndex == i){
 				label.setOpaque(true);
