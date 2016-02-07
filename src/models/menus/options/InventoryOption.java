6ffbc6e8950b51338dfdea5c514b7524109e96c4
Matthew D Diaz <matthewdiaz10@ufl.edu>
2016-02-07 18:39:09 -0500

    Modify name of files with Equip. Working on Inventory and Equip Views

--- src/models/menus/options/InventoryOption.java
+++ src/models/menus/options/InventoryOption.java
@@ -1,12 +1,14 @@
 package models.menus.options;
 import views.*;
 import controllers.*;
+import models.menus.*;
 
 public class InventoryOption extends Option{
 	public InventoryOption(){
 		name = "Inventory";
 	}
 	public void execute(ViewManager vm, Controller cm){
-		//
+		//cm.setMenu(inventoryMenu);
+		//vm.setInventoryMenuMode(inventoryMenu);
   }
 }
