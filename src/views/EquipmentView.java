549fbeb7f7b39f4308ed0eb27806398f7f0cf4e7
Matthew D Diaz <matthewdiaz10@ufl.edu>
2016-02-07 22:59:56 -0500

    Modify EquipmentView.java

--- src/views/EquipmentView.java
+++ src/views/EquipmentView.java
@@ -4,28 +4,30 @@ import models.menus.*;
 import models.menus.options.Option;
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.BorderFactory;
 
 public class EquipmentView extends MenuView{
 
   public EquipmentView(Menus menu){
     super(menu);
+    setLayout(new GridBagLayout());
   }
 
   protected void render(){
     removeAll();
-    for(int i = 0; i < options.length; i++){
-      JLabel label = new JLabel(options[i]);
-      label.setAlignmentX(Component.CENTER_ALIGNMENT);
-      label.setBackground(Color.GREEN);
-      label.setFont(menuFont);
-      if(currentIndex == i){
-        label.setOpaque(true);
-      }
-      else{
-        label.setOpaque(false);
-      }
-      add(label);
+    Border blackline = BorderFactory.createLineBorder(Color.black);
+
+    JPanel equipmentContainer = new JPanel(new GridLayout(2,5));
+    String imgPath = "../images/avatar.gif";
+    JLabel[] grids = new JLabel[10];
+    for(int i = 0; i < 10; i++){
+      grids[i] = new JLabel(new ImageIcon(imgPath));
+      grids[i].setBorder(blackline);
+      equipmentContainer.add(grids[i]);
     }
+
+    add(equipmentContainer);
 
     revalidate();
     repaint();
