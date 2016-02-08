3923b59131489a94069738ff4b1754e1df15591e
Matthew D Diaz <matthewdiaz10@ufl.edu>
2016-02-08 01:34:45 -0500

    Modify EquipmentView.java UI

--- src/views/EquipmentView.java
+++ src/views/EquipmentView.java
@@ -20,19 +20,25 @@ public class EquipmentView extends MenuView{
     GridBagConstraints gBC = new GridBagConstraints();
     JLabel inventoryTitle = new JLabel("Equipment");
     JLabel[] grids = new JLabel[5];
-    String imgPath = "../images/blank_slot.jpg";
 
+    String imgPath = "../images/blank_slot.jpg";
     gBC.anchor = GridBagConstraints.PAGE_START;
+    gBC.weighty = 0.2;
     inventoryTitle.setFont(menuFont);
     add(inventoryTitle, gBC);
 
     for(int i = 0; i < 4; i++){
       grids[i] = new JLabel(new ImageIcon(imgPath));
-      gBC.insets = new Insets(10,0,0,0); 
+      //gBC.insets = new Insets(10,0,0,0);
       gBC.gridx = 0;
       gBC.gridy = i + 1;
       add(grids[i], gBC);
     }
+
+    grids[4] = new JLabel(new ImageIcon(imgPath));
+    gBC.gridx = 1;
+    gBC.gridy = 2;
+    add(grids[4], gBC);
 
     revalidate();
     repaint();
