3d4c5d1aa0ede59aa331c3f1fcaba5952ecfacd1
audgster <ahs3028@gmail.com>
2016-02-08 00:42:14 -0500

    added some extra items to initialize

--- src/util/HardCodedSpriteInitializer.java
+++ src/util/HardCodedSpriteInitializer.java
@@ -22,6 +22,12 @@ public class HardCodedSpriteInitializer implements ISpriteMapInitializer
 		spriteMap.put("Item_InteractableItem", "../images/Item_Interactable.png");
 		spriteMap.put("Item_ObstacleItem", "../images/Item_Obstacle.png");
 
+		spriteMap.put("Item_Super Sayan SwordEquipableItem", "../images/SuperSayanSword.gif");
+		spriteMap.put("Item_The Boots Of AwesomeEquipableItem", "../images/TheBootsOfAwesome.gif");
+
+        spriteMap.put("Item_Healing Super PotionStatModifyingOneShotItem", "../images/Item_Equipable.png");
+        spriteMap.put("Item_Mediocre Healing PostionStatModifyingConsumableItem", "../images/Item_Equipable.png");
+
         //Entity
         spriteMap.put("Entity_Smasher_NORTH", "../images/Entity_Smasher_N.png");
 		spriteMap.put("Entity_Smasher_NORTHEAST", "../images/Entity_Smasher_NE.png");
