920cadc0c82c5db32fa11736c97f190b4a8738c0
Matthew D Diaz <matthewdiaz10@ufl.edu>
2016-02-08 02:50:08 -0500

    Finish modifying InventoryView.java UI

--- src/views/InventoryView.java
+++ src/views/InventoryView.java
@@ -12,7 +12,7 @@ public class InventoryView extends MenuView{
 
   public InventoryView(Menus menu){
 		super(menu);
-		setLayout( new BorderLayout() );
+		setLayout( new GridBagLayout());
     update();
 	}
 
@@ -20,21 +20,24 @@ public class InventoryView extends MenuView{
     removeAll();
 
     Border grayMatteBorder = BorderFactory.createMatteBorder( 1, 1, 1, 1, Color.black);
-    //Dimension panelD = new Dimension(this.getWidth()/3, this.getHeight() );
+    GridBagConstraints mainGBC = new GridBagConstraints();
 
-    // draw title
+    // draw title Panel
+    JPanel titlePanel = new JPanel();
+    titlePanel.setBorder(grayMatteBorder);
     JLabel title = new JLabel("Inventory");
     title.setAlignmentX(Component.CENTER_ALIGNMENT);
     title.setFont(menuFont);
-    add(title, BorderLayout.PAGE_START);
-    title.setBorder(grayMatteBorder);
+    titlePanel.add(title);
+    mainGBC.gridx = 0;
+    mainGBC.weighty = 0.1;
+    add(title, mainGBC);
+
 
     // draw menu panel
     JPanel menuPanel = new JPanel();
     // put JLabels in a column
     menuPanel.setLayout( new BoxLayout(menuPanel, BoxLayout.Y_AXIS ) );
-    //menuPanel.setPreferredSize(panelD);
-    add(menuPanel, BorderLayout.WEST);
     menuPanel.setBorder(grayMatteBorder);
     for(int i = 0; i < options.length; i++){
       JLabel label = new JLabel(options[i]);
@@ -49,13 +52,19 @@ public class InventoryView extends MenuView{
       }
       menuPanel.add(label);
     }
+    mainGBC.gridy = 1;
+    mainGBC.gridx = 0;
+    mainGBC.weightx = 0.5;
+    mainGBC.fill = GridBagConstraints.BOTH;
+    mainGBC.weighty = 0.9;
+    add(menuPanel, mainGBC);
 
     // draw inventory panel
     JPanel inventoryPanel = new JPanel();
     // put JLabels in a column
     inventoryPanel.setLayout( new BoxLayout(inventoryPanel, BoxLayout.Y_AXIS ) );
-    //inventoryPanel.setPreferredSize(panelD);
-    add(inventoryPanel, BorderLayout.CENTER);
+
+
     inventoryPanel.setBorder(grayMatteBorder);
     for(int i = 0; i < options.length; i++){
       JLabel label = new JLabel(options[i]);
@@ -70,27 +79,36 @@ public class InventoryView extends MenuView{
       }
       inventoryPanel.add(label);
     }
-    gBC.insets = new Insets(10,0,0,0);
+    mainGBC.gridy = 1;
+    mainGBC.gridx = 1;
+    mainGBC.weightx = 0.5;
+    add(inventoryPanel, mainGBC);
+
+
     JPanel equipmentPanel = new JPanel(new GridBagLayout());
-    //equipmentPanel.setPreferredSize(panelD);
     equipmentPanel.setBorder(grayMatteBorder);
-    GridBagConstraints gBC = new GridBagConstraints();
+    GridBagConstraints subGBC = new GridBagConstraints();
     JLabel[] grids = new JLabel[5];
     String imgPath = "../images/blank_slot.jpg";
 
     for(int i = 0; i < 4; i++){
       grids[i] = new JLabel(new ImageIcon(imgPath));
-      gBC.gridx = 0;
-      gBC.gridy = i;
-      equipmentPanel.add(grids[i], gBC);
+      subGBC.insets = new Insets(10,0,0,0);  //top padding
+      subGBC.gridx = 0;
+      subGBC.gridy = i;
+      equipmentPanel.add(grids[i], subGBC);
     }
-
     grids[4] = new JLabel(new ImageIcon(imgPath));
-    gBC.gridx = 1;
-    gBC.gridy = 1;
-    equipmentPanel.add(grids[4], gBC);
+    subGBC.gridx = 1;
+    subGBC.gridy = 1;
+    subGBC.insets = new Insets(10,10,0,0);
+    equipmentPanel.add(grids[4], subGBC);
 
-    add(equipmentPanel, BorderLayout.EAST);
+    mainGBC.gridy = 1;
+    mainGBC.gridx = 2;
+    mainGBC.weightx = 0.5;
+    add(equipmentPanel, mainGBC);
+
     revalidate();
     repaint();
   }
