ec770e1e97d378e3fe0e223ff6a00633639a4297
chausen <clayhausen@gmail.com>
2016-02-08 00:37:32 -0500

    Uploaded piskel file...more work on InventoryView

--- src/views/InventoryView.java
+++ src/views/InventoryView.java
@@ -11,35 +11,49 @@ public class InventoryView extends MenuView{
 
   public InventoryView(Menus menu){
 		super(menu);
-		setLayout( new BorderLayout() );    	       
+		setLayout( new BorderLayout() );
+		update();
 	}
 
   protected void render(){
     removeAll();
 
-    Border grayMatteBorder = BorderFactory.createMatteBorder( 1, 5, 1, 1, Color.gray);
+    Border grayMatteBorder = BorderFactory.createMatteBorder( 1, 1, 1, 1, Color.black);
     
     // draw title
     JLabel title = new JLabel("Inventory");
-    title.setAlignmentX(Component.CENTER_ALIGNMENT);
+    title.setAlignmentX(Component.CENTER_ALIGNMENT);        
     title.setFont(menuFont);
     add(title, BorderLayout.PAGE_START);
     title.setBorder(grayMatteBorder);
     
     // draw menu panel
-    //    Container menuPanel = new Container();    
-    //    add(menuPanel, BorderLayout.LINE_START);
-    //    menuPanel.setBorder(grayMatteBorder);
+    JPanel menuPanel = new JPanel();    
+    add(menuPanel, BorderLayout.LINE_START);
+    menuPanel.setBorder(grayMatteBorder);
+    for(int i = 0; i < options.length; i++){
+      JLabel label = new JLabel(options[i]);
+      label.setAlignmentX(Component.CENTER_ALIGNMENT);
+      label.setBackground(Color.GREEN);
+      label.setFont(menuFont);
+      if(currentIndex == i){
+        label.setOpaque(true);
+      }
+      else{
+        label.setOpaque(false);
+      }
+      menuPanel.add(label);
+    }
 
     // draw inventory panel 
-    //    Container inventoryPanel = new Container();    
-    //    add(inventoryPanel, BorderLayout.CENTER);
-    //    inventoryPanel.setBorder(grayMatteBorder);
+    JPanel inventoryPanel = new JPanel();    
+    add(inventoryPanel, BorderLayout.CENTER);
+    inventoryPanel.setBorder(grayMatteBorder);
 
     // draw options panel
-    //    Container optionsPanel = new Container();    
-    //    add(optionsPanel, BorderLayout.LINE_END);
-    //    optionsPanel.setBorder(grayMatteBorder);
+    JPanel optionsPanel = new JPanel();    
+    add(optionsPanel, BorderLayout.LINE_END);
+    optionsPanel.setBorder(grayMatteBorder);
     
     /*
     for(int i = 0; i < options.length; i++){
