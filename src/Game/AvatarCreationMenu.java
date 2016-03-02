abd218e698ce9b01c2ebfaa134d2337067f61191
Brandon <bduong30@ufl.edu>
2016-03-02 15:22:16 -0500

    fixes

--- src/Game/AvatarCreationMenu.java
+++ src/Game/AvatarCreationMenu.java
@@ -1,28 +1,43 @@
-package game;
-
 package Game;
 
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.geometry.Pos;
+import javafx.geometry.Insets;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 
 
 
-
-public class MainMenu{
-	public MainMenu(){
+public class AvatarCreationMenu{
+	public AvatarCreationMenu(){
 
 	}
 
 	public void createScene(Stage primaryStage){
-
-	}
-
-	private GridPane createLayout(){
+		Label text = new Label("Testing Chalk Font");
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
-        return populateLayout(grid);
+		grid.add(text,1,1);
+
+
+
+        Scene scene = new Scene(grid, 300, 275);
+        primaryStage.setScene(scene);
+        scene.getStylesheets().add
+            (AvatarCreationMenu.class.getResource("AvatarCreationMenu.css").toExternalForm());
+        primaryStage.show();
 	}
+
+
 }
