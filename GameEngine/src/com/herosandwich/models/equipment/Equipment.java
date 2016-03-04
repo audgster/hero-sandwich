a3ff0a137b3e5a995692e127cffbd71ce4f68510
audgster <ahs3028@gmail.com>
2016-03-04 14:34:54 -0500

    Updates to inventory

--- GameEngine/src/com/herosandwich/models/equipment/Equipment.java
+++ GameEngine/src/com/herosandwich/models/equipment/Equipment.java
@@ -1,28 +1,49 @@
 package com.herosandwich.models.equipment;
 
-import com.herosandwich.models.items.Item;
-import java.util.ArrayList;
-import java.util.List;
+import com.herosandwich.models.items.EquipableItem;
+import com.herosandwich.models.items.TakeableItem;
 
-/**
- * Created by matthewdiaz on 3/4/16.
- */
+import java.util.HashMap;
+
 public class Equipment {
-    private List<Item> equipment;
+    private HashMap<EquipmentSlots, EquipableItem> equipment;
 
     public Equipment(){
-        equipment = new ArrayList();
+        equipment = new HashMap<>();
     }
 
-    public void insertItem(Item item){
-        equipment.add(item);
+    public Equipment(Equipment equipment)
+    {
+        this.equipment = equipment.getEquipment();
     }
 
-    public boolean removeItem(Item item){
-        return  equipment.remove(item);
+    public TakeableItem insertItem(EquipableItem item, EquipmentSlots location)
+    {
+        // TODO logic for checking to see if a weapon can be put in that slot
+        TakeableItem returnItem = null;
+
+        if (equipment.containsKey(location))
+        {
+            returnItem = equipment.get(location);
+            equipment.remove(location);
+        }
+        equipment.put(location, item);
+
+        return returnItem;
     }
 
-    public List<Item> getInventory(){
-        return  equipment;
+    public TakeableItem removeItem(EquipmentSlots location)
+    {
+        TakeableItem returnItem = null;
+
+        if (equipment.containsKey(location))
+            returnItem = equipment.get(location);
+
+        return returnItem;
+    }
+
+    public HashMap<EquipmentSlots, EquipableItem> getEquipment()
+    {
+        return equipment;
     }
 }
--- GameEngine/src/com/herosandwich/models/equipment/EquipmentSlots.java
+++ GameEngine/src/com/herosandwich/models/equipment/EquipmentSlots.java
@@ -11,5 +11,5 @@ public enum EquipmentSlots
     LEFTARM,
     RIGHTARM,
     LEFTHAND,
-    RIGHTHAND,
+    RIGHTHAND
 }
