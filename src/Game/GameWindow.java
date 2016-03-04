847c1bbded8cc9d2c2808a090d9191a6264ca896
Brandon <bduong30@ufl.edu>
2016-03-03 22:06:03 -0500

    stuff

--- src/Game/GameWindow.java
+++ src/Game/GameWindow.java
@@ -10,18 +10,13 @@ public class GameWindow extends Application{
     }
     @Override
     public void start(Stage primaryStage) {
-<<<<<<< HEAD
-        // AvatarCreationMenu acm = new AvatarCreationMenu();
-        // acm.createScene(primaryStage);
+         AvatarCreationMenu acm = new AvatarCreationMenu();
+         //acm.createScene(primaryStage);
         // MainMenu mm = new MainMenu();
         // mm.createScene(primaryStage);
         AreaView av = new AreaView();
-        av.createScene(primaryStage);
-=======
-        AvatarCreationMenu ac = new AvatarCreationMenu();
-        //ac.createScene(primaryStage);
-        InventoryMenu inventoryMenu = new InventoryMenu();
-        inventoryMenu.renderMenu(primaryStage);
->>>>>>> 8924076d9c1cd8f8003e4fbed7a9fd2fb75b5d2f
+        //av.createScene(primaryStage);
+        InventoryMenu im = new InventoryMenu();
+        im.renderMenu(primaryStage);
     }
 }
--- src/Game/MainMenu.java
+++ src/Game/MainMenu.java
@@ -1,79 +1,76 @@
-package Game;
+// package Game;
 
-import javafx.scene.layout.HBox;
-import javafx.stage.Stage;
-import javafx.scene.control.Label;
-import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
-import javafx.geometry.Pos;
-import javafx.geometry.Insets;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
-import javafx.event.ActionEvent;
+// import javafx.scene.layout.HBox;
+// import javafx.stage.Stage;
+// import javafx.scene.control.Label;
+// import javafx.scene.Scene;
+// import javafx.scene.layout.GridPane;
+// import javafx.geometry.Pos;
+// import javafx.geometry.Insets;
+// import javafx.scene.image.Image;
+// import javafx.scene.image.ImageView;
+// import javafx.scene.control.Button;
+// import javafx.scene.layout.VBox;
+// import javafx.event.ActionEvent;
 
-public class MainMenu{
+// public class MainMenu{
 
-<<<<<<< HEAD
-	public void createScene(Stage primaryStage){
+// 	public void createScene(Stage primaryStage){
+// 	}
+// 	public void renderMenu(Stage primaryStage){
+// 		GridPane layout = createLayout();
+// 		Scene scene = new Scene(layout, 600, 500);
+// 		primaryStage.setScene(scene);
+//         scene.getStylesheets().add(MainMenu.class.getResource("MainMenu.css").toExternalForm());
+//         primaryStage.show();
+// 	}
 
-=======
-	public void renderMenu(Stage primaryStage){
-		GridPane layout = createLayout();
-		Scene scene = new Scene(layout, 600, 500);
-		primaryStage.setScene(scene);
-        scene.getStylesheets().add(MainMenu.class.getResource("MainMenu.css").toExternalForm());
-        primaryStage.show();
-	}
+// 	private GridPane createLayout(){
+//         GridPane grid = new GridPane();
+//         grid.setAlignment(Pos.CENTER);
+//         grid.setHgap(10);
+//         grid.setVgap(10);
+//         grid.setPadding(new Insets(25, 25, 25, 25));
+//         return populateLayout(grid);
+// 	}
 
-	private GridPane createLayout(){
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(25, 25, 25, 25));
-        return populateLayout(grid);
-	}
+// 	private GridPane populateLayout(GridPane layout){
+// 		//creates main menu image
+// 		//Image image = new Image("Game/gameLogo.gif");
+// 		addMainMenuImage(layout);
 
-	private GridPane populateLayout(GridPane layout){
-		//creates main menu image
-		//Image image = new Image("Game/gameLogo.gif");
-		addMainMenuImage(layout);
+// 		setMenuTitle(layout, "Hero Sandwich");
 
-		setMenuTitle(layout, "Hero Sandwich");
+// 		addMainMenuOptions(layout);
 
-		addMainMenuOptions(layout);
+// 		return layout;
+// 	}
 
-		return layout;
-	}
+// 	private void addMainMenuImage(GridPane layout) {
+// 		HBox imageBox = new HBox();
+// 		ImageView image = new ImageView();
+// 		image.setFitWidth(200);
+// 		image.setFitHeight(200);
+// 		image.setId("image");
+// 		imageBox.getChildren().add(image);
+// 		imageBox.setAlignment(Pos.CENTER);
+// 		layout.add(imageBox, 0, 0);
+// 	}
 
-	private void addMainMenuImage(GridPane layout) {
-		HBox imageBox = new HBox();
-		ImageView image = new ImageView();
-		image.setFitWidth(200);
-		image.setFitHeight(200);
-		image.setId("image");
-		imageBox.getChildren().add(image);
-		imageBox.setAlignment(Pos.CENTER);
-		layout.add(imageBox, 0, 0);
-	}
+// 	private void addMainMenuOptions(GridPane layout) {
+// 		VBox vBox = new VBox();
+// 		vBox = createCenteredBottomButton(vBox, "New Game");
+// 		vBox = createCenteredBottomButton(vBox, "Load Game");
+// 		vBox = createCenteredBottomButton(vBox, "Exit");
+// 		layout.add(vBox,0,3);
+// 	}
 
-	private void addMainMenuOptions(GridPane layout) {
-		VBox vBox = new VBox();
-		vBox = createCenteredBottomButton(vBox, "New Game");
-		vBox = createCenteredBottomButton(vBox, "Load Game");
-		vBox = createCenteredBottomButton(vBox, "Exit");
-		layout.add(vBox,0,3);
-	}
+// 	private void setMenuTitle(GridPane layout, String title) {
+// 		Label gameTitle = new Label(title);
+// 		VBox vBoxTemp = new VBox();
+// 		vBoxTemp.setAlignment(Pos.BOTTOM_CENTER);
+// 		vBoxTemp.getChildren().add(gameTitle);
+// 		layout.add(vBoxTemp,0,1);
+// 	}
 
-	private void setMenuTitle(GridPane layout, String title) {
-		Label gameTitle = new Label(title);
-		VBox vBoxTemp = new VBox();
-		vBoxTemp.setAlignment(Pos.BOTTOM_CENTER);
-		vBoxTemp.getChildren().add(gameTitle);
-		layout.add(vBoxTemp,0,1);
->>>>>>> 8924076d9c1cd8f8003e4fbed7a9fd2fb75b5d2f
-	}
-
-}
+// }
