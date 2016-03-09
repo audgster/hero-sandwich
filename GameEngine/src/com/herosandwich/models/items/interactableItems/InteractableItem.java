b406ca4710acf0f9fdaf34ee55591ade80db57aa
Matthew D Diaz <matthewdiaz10@ufl.edu>
2016-03-09 16:29:32 -0500

    Modify Item strucutre to include obstacle, interactable, and oneshot item

---
+++ GameEngine/src/com/herosandwich/models/items/interactableItems/InteractableItem.java
@@ -0,0 +1,9 @@
+package com.herosandwich.models.items.interactableItems;
+
+import com.herosandwich.models.items.Item;
+
+public class InteractableItem extends Item {
+  public InteractableItem(String name){
+      super(name);
+  }
+}
