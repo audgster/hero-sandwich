6a91c47c44e078bc8d61210c8dba69d01fbb3087
mburgunder <mitchellburgunder@yahoo.com>
2016-03-09 17:53:21 -0500

    Fixed map constructor from being derpy

--- GameEngine/src/com/herosandwich/models/map/Map.java
+++ GameEngine/src/com/herosandwich/models/map/Map.java
@@ -11,13 +11,15 @@ public class Map {
     private PositionHex[][] positionArray;
 
     public Map(int size){
-        //this is messed up, will fix soon
+        if(size < 0){
+            throw new IllegalArgumentException("Map size cannot be negative");
+        }
         positionArray = new PositionHex[2 * size + 1][2 * size + 1];
         for(int q = -size; q <= size; q++){
             int rStart = -size - Math.min(q, 0);
             int rEnd = size - Math.max(q, 0);
             for(int r = rStart; r <= rEnd; r++){
-                positionArray[size + q][size + q + Math.min(r, 0)] = new PositionHex(q, r);
+                positionArray[size + q][size + r] = new PositionHex(q, r);
             }
         }
     }
