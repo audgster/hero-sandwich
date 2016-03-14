3e5a265461fd7b0bd2a1c6def76a1f1a8e487f56
Matthew D Diaz <matthewdiaz10@ufl.edu>
2016-03-13 21:50:39 -0400

    Huge modification to Occupation & its subclasses

--- GameEngine/src/com/herosandwich/models/occupation/Occupation.java
+++ GameEngine/src/com/herosandwich/models/occupation/Occupation.java
@@ -4,14 +4,32 @@ package com.herosandwich.models.occupation;
 import com.herosandwich.models.entity.Character;
 import com.herosandwich.models.entity.Skill;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public abstract class Occupation{
     protected Character owner;
+    protected HashMap<Skill, Integer> occupationSkills;
+    protected List<Skill> learned_skills;
 
-    public Occupation(){}
+    public Occupation(){
+        occupationSkills = new HashMap<>();
+        learned_skills = new ArrayList<>();
+        learned_skills.add(Skill.BIND_WOUNDS);
+        learned_skills.add(Skill.BARGAIN);
+        learned_skills.add(Skill.OBSERVATION);
+    }
 
     public Occupation(Character owner){
         this.owner = owner;
+        occupationSkills = new HashMap<>();
+        learned_skills = new ArrayList<>();
+        learned_skills.add(Skill.BIND_WOUNDS);
+        learned_skills.add(Skill.BARGAIN);
+        learned_skills.add(Skill.OBSERVATION);
     }
+
 
     public boolean successfulAction(int skillLevel){
         int prob = (int) Math.ceil(Math.random() * 100) + skillLevel;
@@ -42,8 +60,19 @@ public abstract class Occupation{
         }
     }
 
+    public final void updateOccupationSkills(){
+        for(Skill skill : learned_skills){
+            if(occupationSkills.containsKey(skill)){
+                occupationSkills.replace(skill, owner.getNumberOfSkillPoints(skill));
+            }else{
+                occupationSkills.put(skill, owner.getNumberOfSkillPoints(skill));
+            }
+        }
+    }
 
-    public abstract void updateOccupationSkills();
+    public int getLevelOfSkill(Skill skill){
+        return occupationSkills.get(skill);
+    }
 
     public void setOwner(Character owner){
         this.owner = owner;
--- GameEngine/src/com/herosandwich/models/occupation/Sneak.java
+++ GameEngine/src/com/herosandwich/models/occupation/Sneak.java
@@ -6,30 +6,28 @@ import com.herosandwich.models.entity.Skill;
 import com.herosandwich.models.inventory.Inventory;
 import com.herosandwich.models.items.takeableItems.TakeableItem;
 import com.herosandwich.models.items.takeableItems.equipableItems.weapons.sneakWeapons.SneakWeapon;
-import com.herosandwich.models.map.Map;
-import com.herosandwich.models.map.aoe.InstantDamageAoETrap;
 import com.herosandwich.models.map.aoe.Trap;
-import com.herosandwich.util.visitor.TileVisitor;
-
 import java.util.List;
 
 public class Sneak extends Occupation{
-    private int creepSkill;
-    private int detectionSkill;
-    private int removeTrapSkill;
-    private int pickPocketSkill;
-    private int rangedWeaponSkill;
 
     public Sneak(){
-        this.creepSkill = 0;
-        this.detectionSkill = 0;
-        this.removeTrapSkill = 0;
-        this.pickPocketSkill = 0;
-        this.rangedWeaponSkill = 0;
+        super();
+        learned_skills.add(Skill.CREEP);
+        learned_skills.add(Skill.DETECTION);
+        learned_skills.add(Skill.REMOVE_TRAP);
+        learned_skills.add(Skill.RANGED_WEAPON);
+        //when a character is created all skill levels are zero
+        updateOccupationSkills();
     }
 
     public Sneak(Character owner){
         super(owner);
+        learned_skills.add(Skill.CREEP);
+        learned_skills.add(Skill.DETECTION);
+        learned_skills.add(Skill.REMOVE_TRAP);
+        learned_skills.add(Skill.RANGED_WEAPON);
+        //when a character is created all skill levels are zero
         updateOccupationSkills();
     }
 
@@ -38,12 +36,12 @@ public class Sneak extends Occupation{
     //For now creep mode will last for 5 seconds after being activated. After
     //those 5 seconds are complete the stats go back to normal
     public void creep(){
-        if(successfulAction(this.creepSkill) ){
+        //if(successfulAction(this.creepSkill) ){
             Thread thread = new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    int creepIncrease = 10 + creepSkill;
-                    owner.allocateSkillPoints(Skill.CREEP, creepIncrease);
+             //       int creepIncrease = 10 + creepSkill;
+                  //  owner.allocateSkillPoints(Skill.CREEP, creepIncrease);
                     owner.modifyMovement(-5);
                     try{
                         Thread.sleep(5000);
@@ -51,31 +49,30 @@ public class Sneak extends Occupation{
                         e.printStackTrace();
                     }
                     //reset value of creep skill and movement speed back to normal
-                    owner.allocateSkillPoints(Skill.CREEP, -creepIncrease);
+                  //  owner.allocateSkillPoints(Skill.CREEP, -creepIncrease);
                     owner.modifyMovement(5);
                 }
             });
             thread.start();
-        }
-
+       // }
     }
 
     //If successful discoveries a trap!
     public void detectTrap(){
-        if(successfulAction(this.detectionSkill) ){
+        //if(successfulAction(this.detectionSkill) ){
 
-        }
+    //    }
     }
 
 
     //If successful deactivates a trap!
     public void removeTrap(Trap trap){
-        if(successfulAction(this.removeTrapSkill) ){
+        //if(successfulAction(this.removeTrapSkill) ){
             if(trap.isDiscovered()){
 
             }
             trap.deactivate();
-        }
+        //}
     }
 
     /*
@@ -83,35 +80,25 @@ public class Sneak extends Occupation{
       * and insert it to owner's inventory
      */
     public void pickPocket(Npc npc){
-        if(successfulAction(this.pickPocketSkill) ){
+       // if(successfulAction(this.pickPocketSkill) ){
             Inventory npcInventory = npc.getInventory();
             List npcInventoryList = npcInventory.getInventory();
 
             int randomSlot = (int) Math.ceil(Math.random() * (npcInventoryList.size() - 1));
             TakeableItem npcItem = npcInventory.removeItem((TakeableItem) npcInventoryList.get(randomSlot));
             owner.insertItemToInventory(npcItem);
-        }
+        //}
     }
 
     //this will get fixed with damageCalculator
     public int rangedWeaponAttack(SneakWeapon weapon){
         int damage = 0;
-        if(successfulAction(this.rangedWeaponSkill) ){
+        //if(successfulAction(this.rangedWeaponSkill) ){
             damage += owner.getOffensiveRating(); //damage from entity
             //damage += weapon.getWeaponsOffensiveRating(); // additional damage from weapon
-        }
+       // }
         return damage;
     }
-
-    @Override
-    public void updateOccupationSkills(){
-        this.creepSkill = owner.getNumberOfSkillPoints(Skill.CREEP);
-        this.detectionSkill = owner.getNumberOfSkillPoints(Skill.DETECTION);
-        this.pickPocketSkill = owner.getNumberOfSkillPoints(Skill.PICK_POCKET);
-        this.rangedWeaponSkill = owner.getNumberOfSkillPoints(Skill.RANGED_WEAPON);
-        this.removeTrapSkill = owner.getNumberOfSkillPoints(Skill.REMOVE_TRAP);
-    }
-
 
     @Override
     public String toString() {
