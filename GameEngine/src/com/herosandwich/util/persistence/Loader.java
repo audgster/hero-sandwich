d4f5915dcee5e795b9bee96c86626672b522a491
audgster <ahs3028@gmail.com>
2016-03-14 00:00:55 -0400

    updated loader to have load from init file and load from save

--- GameEngine/src/com/herosandwich/util/persistence/Loader.java
+++ GameEngine/src/com/herosandwich/util/persistence/Loader.java
@@ -1,8 +1,11 @@
 package com.herosandwich.util.persistence;
 
 import com.herosandwich.models.Game;
+import com.herosandwich.models.entity.Character;
 
 public interface Loader
 {
     Game loadGame();
+
+    Game loadGame(Character newPlayer);
 }
